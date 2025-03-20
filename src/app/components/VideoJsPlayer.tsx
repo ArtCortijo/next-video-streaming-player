a76ea986fdbc13dd64e1eff106136fdff9e6231b
acortijo84 <artcortijo@gmail.com>
2025-03-20 10:28:24 -0400

    Video streaming using imagekitio-next

--- src/app/components/VideoJsPlayer.tsx
+++ src/app/components/VideoJsPlayer.tsx
@@ -1,34 +1,20 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+import { IKVideo } from 'imagekitio-next';
 
 const VideoJsPlayer = () => {
-	const videoRef = useRef<any>(null);
-
-	useEffect(() => {
-		const videoElement = document.createElement('video-js');
-		if (videoRef.current) {
-			videoRef.current.appendChild(videoElement);
-			videojs(videoElement, {
-				controls: true,
-				autoplay: false,
-				width: 1024,
-				sources: [
-					{
-						// Adaptive bitrate streaming
-						src: 'https://ik.imagekit.io/2tjs4zugh/test.mp4/ik-master.m3u8?tr=sr-240_360_480_720_1080',
-						type: 'application/x-mpegURL',
-					},
-				],
-			});
-		}
-	}, []);
-
 	return (
 		<div data-vjs-player>
-			<div ref={videoRef} />
+			<IKVideo
+				urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT}
+				path='/test.mp4'
+				controls
+				transformation={[
+					{
+						width: 1024,
+					},
+				]}
+			/>
 		</div>
 	);
 };
